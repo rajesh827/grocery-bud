b1bfaaa7fcdc5321b238b97f9dd533d68f551c67
rajesh827 <rajeshbhandari2075@gmail.com>
2026-01-25 22:10:08 +0545

    added app js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+import { groceryItems } from "./data.js";
+import { createItems } from "./items.js";
+
+let items = groceryItems;
+
+// Render App
+function render() {
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+
+  const itemsElement = createItems(items);
+  app.appendChild(itemsElement);
+}
+
+render();
