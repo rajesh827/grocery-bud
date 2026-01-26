1667caa529fcbd033e724b8f0041d0044e274cfb
rajesh827 <rajeshbhandari2075@gmail.com>
2026-01-26 12:38:28 +0545

    added remove item function

--- js/app.js
+++ js/app.js
@@ -23,3 +23,9 @@ export function editCompleted(itemId) {
   });
   render();
 }
+
+export function removeItem(itemId) {
+  items = items.filter((item) => item.id !== itemId);
+  render();
+  setTimeout(() => alert("Item Deleted Successfully!"), 0);
+}
