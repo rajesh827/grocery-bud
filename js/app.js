41baee41953c135975c28fcbb30ddf30bddc7278
rajesh827 <rajeshbhandari2075@gmail.com>
2026-01-26 18:57:45 +0545

    added item function

--- js/app.js
+++ js/app.js
@@ -18,6 +18,23 @@ function render() {
 
 render();
 
+// Generate unique ID
+function generateId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+// Add Item Function
+export function addItem(itemName) {
+  const newItem = {
+    name: itemName,
+    completed: false,
+    id: generateId(),
+  };
+  items = [...items, newItem];
+  render();
+  setTimeout(() => alert("Item Added Successfully!"), 0);
+}
+
 export function editCompleted(itemId) {
   items = items.map((item) => {
     if (item.id === itemId) {
